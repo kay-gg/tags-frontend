3c97198d03c63ec704cf3d945e059b79b639f29f
Kyle <140885143+kyle-gardner@users.noreply.github.com>
2024-09-02 16:37:29 -0400

    BIG! filtering files based on tags working! its ugly though

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,29 @@
-use std::process::Command;
 
-use iced::*;
+use iced::widget;
+use iced::widget::scrollable::Direction;
+
+use iced::Length;
+use std::collections::HashMap;
+use std::process::Command;
 use iced::widget::row;
 use iced::widget::column;
-
-use serde::Serialize;
+use iced::Element;
+use iced::Sandbox;
 use widget::button;
-use widget::container;
-use widget::scrollable::*;
-use widget::space;
 use widget::Scrollable;
 use iced::widget::text;
-
 use tag_fs::Filesystem;
-use tag_fs::Tag;
-use widget::Space;
 struct App {
-	tags: Filesystem,
+	fs: Filesystem,
+	// <tag name, Button clicked state>
+	tags: HashMap<String, bool>,
+	files: Vec<String>,
 }
 #[derive(Debug)]
 #[derive(Clone)]
 enum Message {
 	Help,
-	CursorUp,
-	CursorLeft,
-	CursorDown,
-	CursorRight,
+	TagClicked(String),
 }
 impl Sandbox for App {
 	type Message = Message;
@@ -35,28 +33,51 @@ impl Sandbox for App {
 			Message::Help => {
 				// Make popup box with controls or something of said nature and the like.
 			},
-
-			_ => unimplemented!(),
+			Message::TagClicked(name) => {
+				// change state of button
+				let b = self.tags.get(&name).unwrap();
+				self.tags.insert(name, !b);
+				// filter files, and replace them
+				let tags_selected: Vec<String> = {
+					self.tags.iter()
+						.filter(|siiyuh| siiyuh.1 == &true)
+						.map(|(k, _)| k.clone())
+						.collect()
+				};
+				let new_files = self.fs.filter(tags_selected).unwrap();
+				self.files = new_files;
+			},
 		}
 	}
 	fn view(&self) -> Element<'_, Self::Message> {
+		let tag_bar = column(self.tags.iter().map(|(name, _)| {
+			button(text(name))
+			 	.on_press(Message::TagClicked(name.into()))
+				.width(Length::Fill)
+				.into()
+		})).spacing(10).padding(5);
 
-		let tagnames = self.tags.return_tags().unwrap();
-		let filenames = self.tags.return_files().unwrap();
+		// let tag_bar = column((0..tagnames.len()).map(|i| { 
+		// 	// horizontal alignment isnt doing anything....
+		// 	button(text(format!("{}", tagnames[i])).horizontal_alignment(iced::alignment::Horizontal::Center))
+		// 		.width(Length::Fill)
+		// 		.on_press(Message::TagClicked)
+		// 		.into()
+		// })).spacing(10).padding(5).align_items(iced::Alignment::Center).width(Length::Fill);
 
-		let tag_bar = column((0..tagnames.len()).map(|i| text(format!("{}", tagnames[i])).into())).spacing(10);
-		let file_bar = column((0..filenames.len()).map(|i| text(format!("{}", filenames[i])).into())).spacing(10);
+		let file_bar = column((0..self.files.len()).map(|i| text(format!("{}", self.files[i])).into())).spacing(10);
 		
 		let top_half = row![
 			Scrollable::new(tag_bar)
 				.width(230).height(Length::Fill)
-				.direction(Direction::Vertical(Properties::new())), // change .style() of these 
+				.direction(Direction::Vertical(iced::widget::scrollable::Properties::new())), // change .style() of these 
 			Scrollable::new(file_bar)
 				.width(Length::Fill).height(Length::Fill)
-				.direction(Direction::Vertical(Properties::new())),
+				.direction(Direction::Vertical(iced::widget::scrollable::Properties::new())),
 		]
 			.height(Length::Fill)
 			.spacing(10);
+		
 		return top_half.into();
 	}
 	fn new() -> Self {
@@ -64,19 +85,48 @@ impl Sandbox for App {
 		let _ = Command::new("tag-backend")
 			.output()
 			.expect("tag backend not installed");
-
 		// Try to load files
 		let tags = Command::new("tag-backend").arg("-F").output().expect("Something went wrong with -F");
 		let tags = String::from_utf8_lossy(&tags.stdout);
-
 		let fs = serde_json::from_str::<Filesystem>(&tags).unwrap();
-		return App {tags: fs};
+
+		// Hashmap full of tags and their state
+		let mut tag_hs: HashMap<String, bool> = HashMap::new();
+		let tags = fs.return_tags().unwrap();
+		for s in tags {
+			tag_hs.insert(s, false);
+		}
+
+		let files = fs.return_files().unwrap().clone();
+
+		return App {
+			fs: fs,
+			tags: tag_hs,
+			files: files,
+		};
 	}
 	fn title(&self) -> String {"Tagging GUI".into()}
-	fn theme(&self) -> Theme {
+	fn theme(&self) -> iced::Theme {
 		iced::Theme::CatppuccinFrappe
 	}
 }
-fn main() -> iced::Result {
-	<App as iced::Sandbox>::run(Settings::default())
+/* 
+struct TagStyle;
+impl button::StyleSheet for TagStyle {
+	type Style = iced::Theme;
+
+	fn active(&self, style: &Self::Style) -> button::Appearance {
+		button::Appearance { 
+			shadow_offset: iced::Vector::default(),
+			background: Some(Background::Color(Color::BLACK)), 
+			text_color: (),
+			border: (),
+			shadow: () 
+		}
+	}
 }
+	*/
+fn main() -> iced::Result {
+	<App as iced::Sandbox>::run(iced::Settings::default())
+}
+
